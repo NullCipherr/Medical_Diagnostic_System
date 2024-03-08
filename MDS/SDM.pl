a63c6d396b3b40f77c2b9041425602f5f4cfda7d
Andrei Costa <141879308+NullCipherr@users.noreply.github.com>
2024-03-08 13:22:04 -0300

    Update SDM.pl

--- MDS/SDM.pl
+++ MDS/SDM.pl
@@ -1,12 +1,3 @@
-%╔══════════════════════════════════════════════════════════════════════╗%
-%║                  Alunos                             RA               ║%
-%╠══════════════════════════════════════════════════════════════════════╣%
-%║       Andrei Roberto da Costa                     107975             ║%
-%║       Joao Gilberto Pelisson Casagrande           112684             ║%
-%║       Rodrigo Vieira de Vasconcelos               112680             ║%
-%╚══════════════════════════════════════════════════════════════════════╝%
-
-
 %% carregar_dependencias is det.
 %
 % Carrega os arquivos de dependencias, como sintomas, etc.
